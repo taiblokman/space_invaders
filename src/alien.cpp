d3f987a3220438933b77a2da42b883e3dae209fe
taiblokman <taiblokman@gmail.com>
2024-08-03 12:52:58 +0800

    initial alien class

---
+++ src/alien.cpp
@@ -0,0 +1,31 @@
+#include "alien.hpp"
+
+Alien::Alien(int type, Vector2 position)
+{
+    this -> type = type;
+    this -> position = position;
+
+    switch (type) {
+        case 1:
+            image = LoadTexture("Graphics/alien_1.png");
+            break;
+        case 2:
+            image = LoadTexture("Graphics/alien_2.png");
+            break;
+        case 3:
+            image = LoadTexture("Graphics/alien_3.png");
+            break;                        
+        default:
+            image = LoadTexture("Graphics/alien_1.png");
+            break;            
+    }
+}
+
+void Alien::Draw() {
+
+    DrawTextureV(image, position, WHITE);
+}
+
+int Alien::GetType(){
+    return type;
+}
